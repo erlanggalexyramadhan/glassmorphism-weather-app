f61e4de2995ece176c976296062b7ad582f4aab1
Erlangga Lexy Ramadhan <erlanggalexyramadhan25@gmail.com>
2024-07-29 13:47:00 +0700

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: "/glassmorphism-weather-app",
+    assetPrefix: "/glassmorphism-weather-app/",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
